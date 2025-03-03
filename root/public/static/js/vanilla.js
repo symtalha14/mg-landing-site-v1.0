0ab971c7635d5b395f28b4b4b1b329b74c464785
smtalha682 <smtalha682@gmail.com>
2025-03-03 20:52:38 +0530

    changes

--- root/public/static/js/vanilla.js
+++ root/public/static/js/vanilla.js
@@ -1,69 +1,99 @@
 window.onload = () => {
 
 
-    const SLIDES_COUNT = 2;
-    const SLIDE_INTERVAL = 3000;
-    
+    var SLIDE_INTERVAL = 6000;
+
     var nextSlideBtn = document.querySelector("#next");
     var previousSlideBtn = document.querySelector("#previous");
-    
+    var slides = Array.prototype.slice.call(document.querySelectorAll(".slide"));
+    const SLIDES_COUNT = 3;
+
     var slideStart = 1;
 
     var currentSlide = document.querySelector("[data-slide='" + slideStart + "']");
-    var lastSlide = -1;
+    var lastSlide = 1;
 
     var forwards = true;
-    
+
     var handler = () => {
 
-        changeSlide();
         slideStart++;
+        changeSlide(1);
 
     };
 
     // var interval = setInterval(handler, SLIDE_INTERVAL);
 
-    // previousSlideBtn.addEventListener("click", (e)=>{
-    //     clearInterval(interval);
-        
-    //     changeSlide();
-    //     slideStart--;
-    // });
+    previousSlideBtn.addEventListener("click", (e) => {
+        // clearInterval(interval);
+        // SLIDE_INTERVAL += 1000;
+        if (slideStart > 1) {
+            slideStart--;
 
-    // nextSlideBtn.addEventListener("click", (e)=>{
-    //     clearInterval(interval);
-    //     slideStart++;
-    //     changeSlide();
-    // });
+            changeSlide(0);
+        }
+    });
+
+    nextSlideBtn.addEventListener("click", (e) => {
+        // clearInterval(interval);
+        // SLIDE_INTERVAL += 1000;
+        slideStart++;
+        changeSlide(1);
+    });
 
 
-    const changeSlide = ()=>{
-
-        if(slideStart == 0){
+    const changeSlide = (direction) => {
+        console.log("Changing slide");
+        console.log("Slide Start=" + slideStart);
+        console.log("Last Slide=" + lastSlide);
+        console.log("-----");
+        if (slideStart == 0) {
+            console.log("zero");
+            slideStart = 1;
+            lastSlide = 0;
             return;
         }
 
-        if(slideStart == (SLIDES_COUNT)){
+        if (slideStart == (SLIDES_COUNT + 1)) {
+            slides.forEach(slide => {
+                slide.classList.remove("animateBackward");
+                slide.classList.remove("animateForward");
+            });
             slideStart = 1;
+            lastSlide = SLIDES_COUNT;
             forwards = !forwards;
         };
-
+        console.log("Slide Start=" + slideStart);
+        console.log("Last Slide=" + lastSlide);
         currentSlide = document.querySelector("[data-slide='" + slideStart + "']");
         console.log(currentSlide);
-        if(currentSlide == null){
+        if (currentSlide == null) {
             return;
         }
-        if(forwards){
-            currentSlide.classList.remove("animateBackward");
-            currentSlide.classList.add("animateForward");
-        }else{
-            currentSlide.classList.remove("animateForward");
-            currentSlide.classList.add("animateBackward");
+        if (direction == 1) {
+            console.log("translateX(-" + (slideStart * 100) + "%);");
+            slides.forEach(slide => {
+                slide.style.transform = "translateX(-" + ((slideStart - 1) * 100) + "%)";
+                // slide.classList.add("animateForward");
+            });
+        } else {
+            console.log("translateX(" + ((slideStart - lastSlide) * 100) + "%);");
+
+            slides.forEach(slide => {
+
+                slide.style.transform = "translateX(-" + ((slideStart - 1) * 100) + "%)";
+                // slide.classList.remove("animateForward");
+            })
         }
 
-        interval = setInterval(handler, SLIDE_INTERVAL);
+        lastSlide = slideStart;
+        // if (SLIDE_INTERVAL > 3000) { SLIDE_INTERVAL -= 1000; }
+        // interval = setInterval(handler, SLIDE_INTERVAL);
 
     }
+
+
+    var slideshowInterval = setInterval(handler, SLIDE_INTERVAL);
 
 
     window.onscroll = function () { myFunction() };
